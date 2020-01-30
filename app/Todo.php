082c82b71a192cd289d00754300979ec7afcce99
shuhei2459 <>
2020-01-30 10:30:16 +0900

    論理削除に変更

--- app/Todo.php
+++ app/Todo.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Todo extends Model
 {
+
     protected $fillable = [       //書き換えられるのはtitleカラムだけ　protected　同じクラス、派生したクラスでも使える
         'title','user_id'
     ];
@@ -14,5 +16,14 @@ class Todo extends Model
     {
     return $this->where('user_id', $id)->get();
     }
+
+    use SoftDeletes;
+
+    /**
+     * 日付へキャストする属性
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
 }
 // ここまで追記
